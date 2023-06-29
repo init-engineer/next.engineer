101634600e4b55eafa6cf41b56e185eb8ecd4b7a
乾太 ₍₍ ◝(･◡･)◟ ⁾⁾ <kantai.developer@gmail.com>
2023-06-29 17:49:09 +0800

    :)

---
+++ resources/js/backend/app.ts
@@ -0,0 +1,16 @@
+import 'alpinejs'
+
+window.Swal = require('sweetalert2');
+
+
+/**
+ * CoreUI at:
+ * https://coreui.io/
+ */
+window.Coreui = require('@coreui/coreui');
+
+/**
+ * Plugins at:
+ * /resources/js/plugins.js
+ */
+require('../plugins');
