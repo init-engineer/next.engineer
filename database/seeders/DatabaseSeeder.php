e8da27ef2bd6a0bf4f7b681fb38dfa728154978a
乾太 ₍₍ ◝(･◡･)◟ ⁾⁾ <kantai.developer@gmail.com>
2024-03-19 22:40:52 +0800

    feat: Install Laravel Jetstream

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@admin.com',
+            'password' => 'secret',
         ]);
+
+        if (app()->environment(['local', 'testing'])) {
+            User::create([
+                'name' => 'Test User',
+                'email' => 'user@user.com',
+                'password' => 'secret',
+            ]);
+        }
     }
 }
