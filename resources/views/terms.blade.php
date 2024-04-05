8e0c62819e149ccad7f04dad7692952addda3ff8
乾太 ₍₍ ◝(･◡･)◟ ⁾⁾ <kantai.developer@gmail.com>
2024-04-05 21:58:23 +0800

    feat: Update Policy and Terms

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -5,7 +5,7 @@
                 <x-authentication-card-logo />
             </div>
 
-            <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
+            <div class="w-full sm:max-w-2xl my-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
                 {!! $terms !!}
             </div>
         </div>
